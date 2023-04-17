4df9e3f33556fbae5848832b15a2f929f1882655
Odran Sourdeval <odran.sourdeval@univ-lille.fr>
2023-04-17 17:19:59 +0200

    add a year filetering option

--- R/l2_grid.R
+++ R/l2_grid.R
@@ -5,7 +5,7 @@
 #' @param overwrite Overwrite the data if it exists. Default: FALSE.
 #' @param nthreads Number of parallel threads. Default: nthreads = 1.
 #' @export
-l2_grid <- function(dir_darni, dir_gridded, overwrite = FALSE, nthreads = 1) {
+l2_grid <- function(dir_darni, dir_gridded, years_only = NULL, overwrite = FALSE, nthreads = 1) {
 
   ## Set up parallelization if nthreads > 1
   parallel = FALSE
@@ -17,6 +17,11 @@ l2_grid <- function(dir_darni, dir_gridded, overwrite = FALSE, nthreads = 1) {
 
   ## List of DARDAR-Nice L2 files
   lf <- list.files(dir_darni, recursive = TRUE, full.names = TRUE, pattern = ".nc")
+
+  ## Filter years if requested
+  if(!is.null(years_only)) {
+    lf <- lf[which(as.numeric(stringr::str_extract(basename(lf), "\\d{4}")) %in% years_only)]
+  }
 
   ## Create one set of gridded files per orbits
   null <- plyr::ldply(lf, darnitools::grid_orbit, dir_out = dir_gridded, overwrite = overwrite, .parallel = parallel)
