e7571aef08cc90877f6547cad4b7ec3de0ded0c1
Odran Sourdeval <odran.sourdeval@univ-lille.fr>
2023-04-19 13:33:19 +0200

    add version comparisons

--- R/l2_grid.R
+++ R/l2_grid.R
@@ -5,7 +5,7 @@
 #' @param overwrite Overwrite the data if it exists. Default: FALSE.
 #' @param nthreads Number of parallel threads. Default: nthreads = 1.
 #' @export
-l2_grid <- function(dir_darni, dir_gridded, years_only = NULL, overwrite = FALSE, nthreads = 1) {
+l2_grid <- function(dir_darni, dir_gridded, years_only = NULL, version_2 = NULL, overwrite = FALSE, nthreads = 1) {
 
   ## Set up parallelization if nthreads > 1
   parallel = FALSE
@@ -26,8 +26,18 @@ l2_grid <- function(dir_darni, dir_gridded, years_only = NULL, overwrite = FALSE
   ## Create one set of gridded files per orbits
   null <- plyr::ldply(lf, darnitools::grid_orbit, dir_out = dir_gridded, overwrite = overwrite, .parallel = parallel)
 
+  ## Create one set of gridded files per orbit comparing two versions
+  if(!is.null(version_2)) {
+    null <-  plyr::ldply(lf, darnitools::grid_orbit_version, v2 = version_2, dir_out = dir_gridded, overwrite = overwrite, .parallel = parallel)
+  }
+
   ## Merge all the orbital files
   null <- darnitools::grid_merge(dir_gridded)
+
+  ## Merge all files for version comparisons
+  if(!is.null(version_2)) {
+    null <- darnitools::grid_merge_version(dir_gridded, v2 = version_2)
+  }
 
   ## Uninitialize the doMC backend
   if(parallel) {
